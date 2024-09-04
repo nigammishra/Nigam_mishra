33a74e1a1a32fe359123d2896ed66f831cfbe12e
nigam <nigammishra826@gmail.com>
2024-09-04 17:50:03 +0530

    change the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../../ThemeContext"; // Adjust path if needed
+import blackLogo from "../../assets/blacklogo.png"; // Light mode logo
+import whiteLogo from "../../assets/whitelogo.png"; // Dark mode logo
 import "./footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const { isDarkMode } = useContext(ThemeContext);
@@ -8,66 +11,102 @@ const Footer = () => {
   return (
     <footer className={`footer ${isDarkMode ? "dark-mode" : ""}`}>
       <div className="footer__container">
-        <h1 className="footer__title">Nigam Mishra</h1>
-        <ul className="footer__list">
-          <li>
-            <a href="/about" className="footer__link">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/work" className="footer__link">
-              Projects
-            </a>
-          </li>
-          <li>
-            {/* <a href="#testimonials" className="footer__link">
-              Testimonial
-            </a> */}
-          </li>
-        </ul>
-        <div className="footer__social">
-          <a
-            href="https://x.com/nigammishra1103"
-            className="footer__social-link"
-            target="_blank" // Opens the link in a new tab
-            rel="noopener noreferrer" // Improves security when opening external links
-          >
-            <i className="uil uil-twitter-alt"></i>
-          </a>
-          <a
-            href="https://www.instagram.com/mr_nigam_8199" // Replace this with your Instagram profile link
-            className="footer__social-link"
-            target="_blank" // Opens the link in a new tab
-            rel="noopener noreferrer" // Improves security when opening external links
-          >
-            <i className="uil uil-instagram"></i>{" "}
-            {/* Class name for Instagram icon */}
-          </a>
+        <div
+          class="row"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div class="col-md-6">
+            <div
+              className="row"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div className="col-12" style={{ display: "contents" }}>
+                <Link to="/" className="">
+                  <img
+                    src={isDarkMode ? whiteLogo : blackLogo}
+                    alt="logo"
+                    style={{ width: "200px" }}
+                  />
+                </Link>
+              </div>
+            </div>
 
-          <a
-            href="https://github.com/nigammishra"
-            className="footer__social-link"
-            target="_blank" // Opens the link in a new tab
-            rel="noopener noreferrer" // Improves security when opening external links
-          >
-            <i className="uil uil-github-alt"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/nigam-mishra-2379642ab/"
-            className="footer__social-link"
-            target="_blank" // Opens the link in a new tab
-            rel="noopener noreferrer" // Improves security when opening external links
-          >
-            <i className="uil uil-linkedin-alt"></i>{" "}
-            {/* Corrected class name for LinkedIn icon */}
-          </a>
-          <a href="https://wa.me/8260821103" className="footer__social-link">
-            <i className="uil uil-whatsapp"></i>
-          </a>
+            <ul className="footer__list">
+              <li>
+                <a href="/about" className="footer__link">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/work" className="footer__link">
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a href="/contact" className="footer__link">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <h1 className="footer__title">Nigam Mishra</h1>
+            <div className="footer__social">
+              <a
+                href="https://x.com/nigammishra1103"
+                className="footer__social-link"
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Improves security when opening external links
+              >
+                <i className="uil uil-twitter-alt"></i>
+              </a>
+              <a
+                href="https://www.instagram.com/mr_nigam_8199" // Replace this with your Instagram profile link
+                className="footer__social-link"
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Improves security when opening external links
+              >
+                <i className="uil uil-instagram"></i>{" "}
+                {/* Class name for Instagram icon */}
+              </a>
+
+              <a
+                href="https://github.com/nigammishra"
+                className="footer__social-link"
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Improves security when opening external links
+              >
+                <i className="uil uil-github-alt"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/nigam-mishra-2379642ab/"
+                className="footer__social-link"
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Improves security when opening external links
+              >
+                <i className="uil uil-linkedin-alt"></i>{" "}
+                {/* Corrected class name for LinkedIn icon */}
+              </a>
+              <a
+                href="https://wa.me/8260821103"
+                className="footer__social-link"
+              >
+                <i className="uil uil-whatsapp"></i>
+              </a>
+            </div>
+          </div>
         </div>
+
         <span className="footer__copy">
-          &copy; FrontEnd Ace. All rights reserved
+          &copy; {new Date().getFullYear()} FrontEnd Ace. All rights reserved.
         </span>
       </div>
     </footer>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { ThemeContext } from '../../ThemeContext'; // Adjust path if necessary
+import { ThemeContext } from "../../ThemeContext"; // Adjust path if necessary
 import "./skill.css";
 import {
   DiJavascript1,
@@ -30,35 +30,122 @@ const Skills = () => {
   const { isDarkMode } = useContext(ThemeContext); // Use useContext to get dark mode state
 
   return (
-    <section className={`skills section ${isDarkMode ? 'dark-mode' : 'light-mode'}`} id="skills">
-      <h2 className={`section__title ${isDarkMode ? 'dark-mode-title' : 'light-mode-title'}`}>Skills</h2>
-      <span className={`section__subtitle ${isDarkMode ? 'dark-mode-subtitle' : 'light-mode-subtitle'}`}>My Technical Skillset</span>
+    <section
+      className={`skills section ${isDarkMode ? "dark-mode" : "light-mode"}`}
+      id="skills"
+    >
+      <h2
+        className={`section__title ${
+          isDarkMode ? "dark-mode-title" : "light-mode-title"
+        }`}
+      >
+        Skills
+      </h2>
+      <span
+        className={`section__subtitle ${
+          isDarkMode ? "dark-mode-subtitle" : "light-mode-subtitle"
+        }`}
+      >
+        My Technical Skillset
+      </span>
       <div className="skills__icons grid container">
-        <DiReact size={50} color="#61DAFB" />
-        <DiNodejs size={50} color="#8CC84B" />
+        <div className="icon-container">
+          <DiReact size={50} color="#61DAFB" />
+          <span className="tooltip-text">React.js</span>
+        </div>
+        <div className="icon-container">
+          <DiNodejs size={50} color="#8CC84B" />
+          <span className="tooltip-text">Nodejs</span>
+        </div>
+        <div className="icon-container">
         <DiCss3 size={50} color="#1572B6" />
+          <span className="tooltip-text">CSS</span>
+        </div>
+        <div className="icon-container">
         <DiHtml5 size={50} color="#E34F26" />
+          <span className="tooltip-text">HTML</span>
+        </div>
+        <div className="icon-container">
         <DiResponsive size={50} color="#E34F26" />
+          <span className="tooltip-text">Responsive</span>
+        </div>
+        <div className="icon-container">
         <DiGithub size={50} color={isDarkMode ? "#FFFFFF" : "#181717"} />
+          <span className="tooltip-text">GitHub</span>
+        </div>
+        <div className="icon-container">
         <DiBootstrap size={50} color="#563D7C" />
+          <span className="tooltip-text">Bootstrap</span>
+        </div>
+        <div className="icon-container">
         <DiJava size={50} color="#F80000" />
+          <span className="tooltip-text">JAVA</span>
+        </div>
+        <div className="icon-container">
         <DiJavascript1 size={50} color="#F7DF1E" />
+          <span className="tooltip-text">Java Script</span>
+        </div>
+        <div className="icon-container">
         <DiMongodb size={50} color="#4DB33D" />
+          <span className="tooltip-text">Mongo DB</span>
+        </div>
+        <div className="icon-container">
         <DiPython size={50} color="#306998" />
+          <span className="tooltip-text">Python</span>
+        </div>
+        <div className="icon-container">
         <DiGit size={50} color="#F1502F" />
+          <span className="tooltip-text">Git</span>
+        </div> 
       </div>
       <span className="section__subtitle" style={{ marginTop: "40px" }}>
         Tools I Use
       </span>
-      <div className={`skills__icons grid container ${isDarkMode ? 'icon-dark-mode' : 'icon-light-mode'}`}>
+      <div
+        className={`skills__icons grid container ${
+          isDarkMode ? "icon-dark-mode" : "icon-light-mode"
+        }`}
+      >
+        <div className="icon-container">
         <SiVisualstudiocode size={50} color="#007ACC" />
+          <span className="tooltip-text">Visual Studio Code</span>
+        </div>
+        <div className="icon-container">
         <SiWindows11 size={50} color="#0078D4" />
+          <span className="tooltip-text">Windows</span>
+        </div>
+        <div className="icon-container">
         <SiMacos size={50} color={isDarkMode ? "#FFFFFF" : "#000000"} />
+          <span className="tooltip-text">MacOS</span>
+        </div>
+        <div className="icon-container">
         <SiCodechef size={50} color="#5B4638" />
+          <span className="tooltip-text">Code Chef</span>
+        </div>
+        <div className="icon-container">
         <SiSharp size={50} color="#C0C0C0" />
+          <span className="tooltip-text">Sharp</span>
+        </div>
+        <div className="icon-container">
         <SiPostman size={50} color="#FF6C37" />
+          <span className="tooltip-text">Post Man</span>
+        </div>
+        <div className="icon-container">
         <SiLeetcode size={50} color="#F3CA40" />
+          <span className="tooltip-text">Leet Code</span>
+        </div>
+        <div className="icon-container">
         <SiNotepadplusplus size={50} color="#5C2D91" />
+          <span className="tooltip-text">Notepad ++</span>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
       </div>
     </section>
   );
