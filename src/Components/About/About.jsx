a594aecf821376836523b044ec5aec58cb1da6e6
nigam <nigammishra826@gmail.com>
2024-09-16 22:00:49 +0530

    github calender add

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,23 +4,24 @@ import AboutImg from "../../assets/nigam.jpg";
 import CV from "../../assets/nigammishra.pdf";
 import Info from "./Info";
 import { RotatingTriangles } from "react-loader-spinner";
+import GitHubCalendar from "react-github-calendar"; // Correct import if using 'react-github-calendar'
 
 const About = () => {
-  const [loading, setLoading] = useState
-  (true);
+  const [loading, setLoading] = useState(true);
 
-  // Simulate loading time with useEffect
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); // Loader will be visible for 3 seconds
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
+
   return (
-    <>{loading ? (
-      <div className="loader-container">
-         <RotatingTriangles
+    <>
+      {loading ? (
+        <div className="loader-container">
+          <RotatingTriangles
             visible={true}
             height="100"
             width="100"
@@ -29,52 +30,63 @@ const About = () => {
             wrapperClass=""
             // color={isDarkMode ? '#ffffff' : '#000000'}  // Change the color based on mode
           />
-      </div>
-    ) : (
-    <section className="about section" id="about">
-      <h2 className="section__title">About Me</h2>
-      <span className="section__subtitle">My introduction</span>
-      <div className="about_container container grid">
-        <img src={AboutImg} className="about__img" alt="" />
-        <div className="about__data">
-          <Info />
-          <p className="about__description">
-          Full Stack Developer , with 2 Years Of Experience Learning
-            From Youtube , I can create web pages with UI / UX  FronEnd & BackEnd user interface,
-            may clients are happy with projects carried out.
-          </p>
-          <a href={CV} className="button button--flex" download="">
-            Download CV{" "}
-            <svg
-              class="button__icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M15.25 22.7502H9.25C3.82 22.7502 1.5 20.4302 1.5 15.0002V9.00024C1.5 3.57024 3.82 1.25024 9.25 1.25024H14.25C14.66 1.25024 15 1.59024 15 2.00024C15 2.41024 14.66 2.75024 14.25 2.75024H9.25C4.64 2.75024 3 4.39024 3 9.00024V15.0002C3 19.6102 4.64 21.2502 9.25 21.2502H15.25C19.86 21.2502 21.5 19.6102 21.5 15.0002V10.0002C21.5 9.59024 21.84 9.25024 22.25 9.25024C22.66 9.25024 23 9.59024 23 10.0002V15.0002C23 20.4302 20.68 22.7502 15.25 22.7502Z"
-                fill="var(--container-color)"
-              ></path>
-              <path
-                d="M22.25 10.7502H18.25C14.83 10.7502 13.5 9.42023 13.5 6.00023V2.00023C13.5 1.70023 13.68 1.42023 13.96 1.31023C14.24 1.19023 14.56 1.26023 14.78 1.47023L22.78 9.47023C22.99 9.68023 23.06 10.0102 22.94 10.2902C22.82 10.5702 22.55 10.7502 22.25 10.7502ZM15 3.81023V6.00023C15 8.58023 15.67 9.25023 18.25 9.25023H20.44L15 3.81023Z"
-                fill="var(--container-color)"
-              ></path>
-              <path
-                d="M13.25 13.7502H7.25C6.84 13.7502 6.5 13.4102 6.5 13.0002C6.5 12.5902 6.84 12.2502 7.25 12.2502H13.25C13.66 12.2502 14 12.5902 14 13.0002C14 13.4102 13.66 13.7502 13.25 13.7502Z"
-                fill="var(--container-color)"
-              ></path>
-              <path
-                d="M11.25 17.7502H7.25C6.84 17.7502 6.5 17.4102 6.5 17.0002C6.5 16.5902 6.84 16.2502 7.25 16.2502H11.25C11.66 16.2502 12 16.5902 12 17.0002C12 17.4102 11.66 17.7502 11.25 17.7502Z"
-                fill="var(--container-color)"
-              ></path>
-            </svg>
-          </a>
         </div>
-      </div>
-    </section>
-     )}
+      ) : (
+        <section className="about section" id="about">
+          <h2 className="section__title">About Me</h2>
+          <span className="section__subtitle">My introduction</span>
+          <div className="about_container container grid">
+            <img src={AboutImg} className="about__img" alt="" />
+            <div className="about__data">
+              <Info />
+              <p className="about__description">
+                Full Stack Developer, with 2 Years Of Experience Learning
+                From Youtube, I can create web pages with UI/UX FrontEnd & BackEnd user interface,
+                many clients are happy with projects carried out.
+              </p>
+              <a href={CV} className="button button--flex" download="">
+                Download CV{" "}
+                <svg
+                  className="button__icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M15.25 22.7502H9.25C3.82 22.7502 1.5 20.4302 1.5 15.0002V9.00024C1.5 3.57024 3.82 1.25024 9.25 1.25024H14.25C14.66 1.25024 15 1.59024 15 2.00024C15 2.41024 14.66 2.75024 14.25 2.75024H9.25C4.64 2.75024 3 4.39024 3 9.00024V15.0002C3 19.6102 4.64 21.2502 9.25 21.2502H15.25C19.86 21.2502 21.5 19.6102 21.5 15.0002V10.0002C21.5 9.59024 21.84 9.25024 22.25 9.25024C22.66 9.25024 23 9.59024 23 10.0002V15.0002C23 20.4302 20.68 22.7502 15.25 22.7502Z"
+                    fill="var(--container-color)"
+                  ></path>
+                  <path
+                    d="M22.25 10.7502H18.25C14.83 10.7502 13.5 9.42023 13.5 6.00023V2.00023C13.5 1.70023 13.68 1.42023 13.96 1.31023C14.24 1.19023 14.56 1.26023 14.78 1.47023L22.78 9.47023C22.99 9.68023 23.06 10.0102 22.94 10.2902C22.82 10.5702 22.55 10.7502 22.25 10.7502ZM15 3.81023V6.00023C15 8.58023 15.67 9.25023 18.25 9.25023H20.44L15 3.81023Z"
+                    fill="var(--container-color)"
+                  ></path>
+                  <path
+                    d="M13.25 13.7502H7.25C6.84 13.7502 6.5 13.4102 6.5 13.0002C6.5 12.5902 6.84 12.2502 7.25 12.2502H13.25C13.66 12.2502 14 12.5902 14 13.0002C14 13.4102 13.66 13.7502 13.25 13.7502Z"
+                    fill="var(--container-color)"
+                  ></path>
+                  <path
+                    d="M11.25 17.7502H7.25C6.84 17.7502 6.5 17.4102 6.5 17.0002C6.5 16.5902 6.84 16.2502 7.25 16.2502H11.25C11.66 16.2502 12 16.5902 12 17.0002C12 17.4102 11.66 17.7502 11.25 17.7502Z"
+                    fill="var(--container-color)"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <h3 className="calendar-title">Days <strong>I Code</strong></h3>
+          <div className="calendar-container" style={{display:'flex',justifyContent:'center'}}>
+            
+            <GitHubCalendar
+              username="nigammishra"
+              blockSize={15}
+              blockMargin={5}
+              color="#000"
+              fontSize={16}
+            />
+          </div>
+        </section>
+      )}
     </>
   );
 };
